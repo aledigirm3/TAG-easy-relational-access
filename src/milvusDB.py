7aea3090b840fb2cd2734201d0bd5164b4f2fcfe
Diggi <alessandro.digirolamo.1937@gmail.com>
2025-05-06 14:42:04 +0200

    fix: milvus class

--- src/milvusDB.py
+++ src/milvusDB.py
@@ -1,9 +1,9 @@
-from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
 from embedder import Embedder
 import numpy as np
 
 class MilvusDB:
-    def __init__(self, collection_name="semantic_search", host="localhost", port="19530"):
+    def __init__(self, collection_name="TAG_search", host="localhost", port="19530"):
         self.collection_name = collection_name
         self.model = Embedder()
         self.emb_dim = self.model.get_embedding_dimension()
@@ -12,7 +12,8 @@ class MilvusDB:
         self._setup_collection()
 
     def _setup_collection(self):
-        if self.collection_name in Collection.list():
+        collections = utility.list_collections()
+        if self.collection_name in collections:
             self.collection = Collection(self.collection_name)
         else:
             fields = [
@@ -29,17 +30,22 @@ class MilvusDB:
                           "index_type": "IVF_FLAT",
                           "params": {"nlist": 128}} # Number of clusters
         )
+        print(f"Milvus collection -> {self.collection}")
 
     def _ensure_loaded(self):
-        if not self.collection.is_loaded:
             self.collection.load()
 
     # For table row
-    def add_texts(self, texts: list[str]):
+    def add_texts(self, texts: list[str], batch_size: int = 1000):
         embeddings = self.model.get_sentence_embedding(texts).tolist()
-        self.collection.insert([texts, embeddings])
 
-    def search(self, query: str, threshold=0.7, top_k=10000):
+        for i in range(0, len(texts), batch_size):
+            batch_texts = texts[i:i+batch_size]
+            batch_embeddings = embeddings[i:i+batch_size]
+
+            self.collection.insert([batch_texts, batch_embeddings])
+
+    def search(self, query: str, threshold=0.3, top_k=10000):
         self._ensure_loaded()
         embedding = self.model.get_sentence_embedding(query).tolist()
         search_params = {"metric_type": "COSINE", 
